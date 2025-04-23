c66d7ea63ffd1d9ae85f23dc3553deae93e7837f
jemert1992 <justin@tapp.co>
2025-04-23 23:34:19 +0000

    commit delete

--- pages/test.js
+++
@@ -1,9 +0,0 @@
-export default function Test() {
-  return (
-    <div>
-      <h1>Test Page</h1>
-      <p>If you can see this page, Next.js routing is working correctly!</p>
-      <p>This confirms that your folder structure is properly set up.</p>
-    </div>
-  )
-}
